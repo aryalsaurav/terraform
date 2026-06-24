c08a124c767ab981f0fc63131b8b5143e9bdd6c6
Saurav Aryal <aryalsaurav01@gmail.com>
2026-06-24 12:12:46 +0545

    feat: iam roles and ecs task service cluster

---
+++ ecs.tf
@@ -0,0 +1,76 @@
+resource "aws_ecs_cluster" "main" {
+  name = "${local.prefix}-cluster"
+
+  tags = merge(
+        local.common_tags,
+        {
+            Name = "${local.prefix}-alb"
+        }
+    ) 
+}
+
+
+resource "aws_ecs_task_definition" "web" {
+    family = "${local.prefix}-web"
+    network_mode = "awsvpc"
+    requires_compatibilities = ["EC2"]
+
+    cpu = 512
+    memory = 1024
+
+    task_role_arn = aws_iam_role.ecs_task_role.arn
+    execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
+    container_definitions = jsonencode([
+        {
+            name = "Web"
+            image = "${aws_ecr_repository.web.repository_url}:latest"
+
+            portMappings =[
+                {
+                    containerPort = 8000
+                    hostPort = 8000
+                }
+
+            ]
+            essential = true
+        }
+    ])
+
+    tags = merge(
+        local.common_tags,
+        {
+            Name = "${local.prefix}-web-task"
+        }
+    )
+}
+
+resource "aws_ecs_service" "server" {
+    name = "${local.prefix}-service"
+    cluster = aws_ecs_cluster.main.id
+    task_definition = aws_ecs_task_definition.web.arn
+
+    desired_count = 2
+
+    launch_type = "EC2"
+
+    network_configuration {
+      subnets = [ 
+        aws_subnet.private["private_a"].id,
+        aws_subnet.private["private_b"].id,
+       ]
+
+       security_groups = [
+            aws_security_group.web_task.id 
+        ]
+    }
+
+    load_balancer {
+        target_group_arn = aws_alb_target_group.server.arn
+        container_name = "server"
+        container_port = 8000
+    }
+
+    depends_on = [ aws_lb_listener.http ]
+
+}
