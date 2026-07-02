fdd6aad3b04f5021e9c07665e80ca3054a0f433f
Saurav Aryal <aryalsaurav01@gmail.com>
2026-07-02 16:56:04 +0545

    fix: cert and hhtps

--- alb.tf
+++ alb.tf
@@ -45,10 +45,40 @@ resource "aws_lb" "server" {
   )
 }
 
+resource "aws_acm_certificate" "server" {
+  domain_name       = "renter.aryalsaurav.com.np"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "server" {
+  certificate_arn = aws_acm_certificate.server.arn
+}
+
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.server.arn
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.server.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate_validation.server.certificate_arn
 
   default_action {
     type             = "forward"
--- outputs.tf
+++ outputs.tf
@@ -76,3 +76,13 @@ output "alb_dns" {
   value = aws_lb.server.dns_name
 
 }
+
+output "acm_validation_record" {
+  value = {
+    for dvo in aws_acm_certificate.server.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
+  }
+}
