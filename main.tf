f0b1e4ec9a01ca8c2e1ad50d013e6211fa344ee0
Saurav Aryal <aryalsaurav01@gmail.com>
2026-07-02 13:51:36 +0545

    fix: state file to s3

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "s3" {
-    bucket       = "${local.prefix}-terraform-state"
-    key          = "${local.prefix}/terraform.tfstate"
-    region       = var.aws_region
     encrypt      = true
     use_lockfile = true
 
